829fc1cfbdd7a8a6ccfd5e6300c4f3bae1a33d1f
DbertelliA <useromegad@gmail.com>
2026-02-16 18:43:22 +0100

    Prueba de prueba del metodo '''sonIguales()'''

---
+++ tests/CadenasAlumnoTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CadenasAlumnoTest {
+
+    @Test
+    void CP_01() {
+        CadenasAlumno cadenasAlumno1 = new CadenasAlumno();
+        assertEquals(true,cadenasAlumno1.sonIguales("hola","hola"));
+    }
+}

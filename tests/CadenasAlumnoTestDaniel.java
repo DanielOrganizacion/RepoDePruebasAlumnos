2907ab470cabe529501c62ee2837c8379e7b7b96
DbertelliA <useromegad@gmail.com>
2026-02-18 18:51:22 +0100

    3 Casos de prueba de Daniel

--- tests/CadenasAlumnoTestDaniel.java
+++ tests/CadenasAlumnoTestDaniel.java
@@ -2,12 +2,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CadenasAlumnoTest {
+class CadenasAlumnoTestDaniel {
+    CadenasAlumno cadenasAlumno1 = new CadenasAlumno();
 
     @Test
     void CP_01() {
-        CadenasAlumno cadenasAlumno1 = new CadenasAlumno();
         assertEquals(true,cadenasAlumno1.sonIguales("hola","hola"));
+    }
+    @Test
+    void CP_02() {
+        assertEquals(false,cadenasAlumno1.sonIguales("hola","halo"));
+    }
+
+    @Test
+    void CP_03() {
         assertEquals(false,cadenasAlumno1.sonIguales("hola","hola1"));
     }
+
 }
